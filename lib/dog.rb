533ee146a65831d4b01afd6e67fded849db1b6d4
Matt Beaudoin <ehxtwo@gmail.com>
2021-03-04 10:07:46 -0800

    commit tree be806fa4fbb35b61be1b85d4a1174ac9d69d3221

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,38 @@
-# Add your code here
+require 'pry'
+
+class Dog
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        self.save
+    end
+
+    def save
+        @@all << self
+    end
+
+    def self.all 
+        @@all
+    end
+
+    def self.print_all
+        all_names = []
+        @@all.each do |name|
+            all_names << name.name
+        end
+        puts all_names
+    end
+
+    def self.clear_all
+        @@all = []
+    end
+end
+
+
+# pluto = Dog.new("pluto")
+# fido = Dog.new("fido")
+
+# binding.pry
